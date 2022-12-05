fb87468fa2b05c9650c7ce91af3fadc6b80361ad
sungbin <dusvlf5950@naver.com>
2022-12-05 21:30:39 +0900

    12.05
    2022

---
+++ 1_my_python/example/11_cell_style.py
@@ -0,0 +1,26 @@
+from openpyxl.styles import Font
+from openpyxl import load_workbook
+wb = load_workbook("sample.xlsx")
+ws = wb.active
+
+
+a1 = ws["A1"]
+b1 = ws["B2"]
+c1 = ws["C1"]
+
+
+
+# A열의 너비를 5로 설정
+ws.column_dimensions["A"].width = 5
+
+
+# 1행의 높이를 50으로 설정
+ws.row_dimensions["1"].height = 50
+
+
+# 스타일 적용
+#글자색은 빨간 이텔릭 두껍게
+a1.font = Font(color = "FF0000",italic = True, bold=True)
+
+wb.save("sample_style.xlsx")
+
